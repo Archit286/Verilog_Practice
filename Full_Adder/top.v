fd84669d30eab37297012d7a06120364ff2e0b1b
Archit Gupta <agupta28_be19@thapar.edu>
2021-12-31 00:20:18 +0530

    Added Full Adder

---
+++ Full_Adder/top.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+module top(
+    input a,b,c,
+    output sum,carry
+    );
+    
+wire t1,t2,t3,t4,t5;
+
+
+xor g1 (t1,a,b);
+xor g3 (sum,t1,c);
+and g2 (t2,a,b);
+and g4 (t3,t1,c);
+or g5 (carry,t3,t2);
+
+endmodule
+
+module tb();
+
+reg a,b,c;
+wire sum,carry;
+
+initial begin
+a = 0;
+b = 0;
+c = 0;
+end
+
+top dut (a,b,c,sum,carry);
+
+always #5 a = ~a;
+always #10 b = ~b;
+always #20 c = ~c;
+
+initial begin
+#200;
+$finish;
+end
+
+endmodule
